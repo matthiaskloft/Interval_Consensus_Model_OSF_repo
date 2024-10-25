b16f35243e3229301c97a2c21dbb49e5458a4062
matthiaskloft <83857345+matthiaskloft@users.noreply.github.com>
2024-10-25 15:30:56 +0200

    update: osf download script

--- src/00_download_big_files_osf.R
+++ src/00_download_big_files_osf.R
@@ -1,26 +1,16 @@
-packages <- c(
-  "here",
-  "osfr"
-)
+packages <- c("here", "osfr")
 
-if (!require("pacman")) install.packages("pacman")
+if (!require("pacman"))
+  install.packages("pacman")
 pacman::p_load(packages, update = F, character.only = T)
 
 
-
-
-# remove existing fits folders
-if (dir.exists(here("fits"))) {
-  unlink(here("fits"), recursive = T)
-}
-if (dir.exists(here("sim_results"))) {
-  unlink(here("sim_results"), recursive = T)
-}
-
-
-# Specify OSF files
+# Specify OSF file
 project <- osf_retrieve_node("https://osf.io/r32by")
-files <- osf_ls_files(project)
-# download
-osf_download(files[], )
+file <- osf_ls_files(project, verbose = TRUE)
 
+# download
+osf_download(file, conflicts = "overwrite", verbose = TRUE)
+# unzip packaged files
+unzip(here("fits_and_sim_results.zip"), overwrite = TRUE)
+unlink(here("fits_and_sim_results.zip"))
